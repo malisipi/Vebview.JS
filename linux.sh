74cebbe8c11a98fb8494f15eeef3be038a3ebae5
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-08-30 17:19:12 +0300

    Update build script

--- linux.sh
+++ linux.sh
@@ -1,7 +1,7 @@
 clear
 mkdir build
 g++ -c libs/webview/webview.cc -std=c++11 $(pkg-config --cflags gtk+-3.0 webkit2gtk-4.0) -o build/webview.o
-v -gc none src/ -enable-globals -o build/vebview.c
+$V_LOC/v -gc none src/ -enable-globals -o build/vebview.c
 gcc -c build/vebview.c -std=gnu99 -o build/vebview.o -I "libs/tinyfd" -I "libs/webview" -I "$V_LOC/thirdparty/cJSON/" $(pkg-config --cflags gtk+-3.0 webkit2gtk-4.0 x11) -I "libs/libkeybinder" -I "$V_LOC/thirdparty/mbedtls/library" -I "$V_LOC/thirdparty/mbedtls/include" -I "$V_LOC/thirdparty/mbedtls/3rdparty/everest/include" -I "$V_LOC/thirdparty/mbedtls/3rdparty/everest/include/everest" -I "$V_LOC/thirdparty/mbedtls/3rdparty/everest/include/everest/kremlib"
 gcc -c "$V_LOC/thirdparty/cJSON/cJSON.c" -std=c99 -o build/cJSON.o
 gcc -c libs/tinyfd/tinyfiledialogs.c -std=c99 -o build/tinyfd.o
